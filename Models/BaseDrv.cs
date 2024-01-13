1f3b7c7f4cad775d74d421d589afafa02da23e68
blaze <blazezhang@liontravel.com>
2024-01-13 22:22:23 +0800

    增設 父層物件 存放 1. 連線字串

---
+++ Models/BaseDrv.cs
@@ -0,0 +1,10 @@
+﻿namespace EupDriversInfo.Models
+{
+    public class BaseDrv
+    {
+        /// <summary>
+        /// 連線字串
+        /// </summary>
+        public readonly string _connectString = "Server=ITTC-04509-0050\\SQLEXPRESS;Database=EupData;User ID=sa;Password=ssmsLion220090;";
+    }
+}
