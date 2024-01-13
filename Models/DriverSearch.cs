c0b9598d62cf9f2675c2409644ba08c9e0ef1fc0
blaze <blazezhang@liontravel.com>
2024-01-13 22:25:29 +0800

    建立查詢頁model

---
+++ Models/DriverSearch.cs
@@ -0,0 +1,34 @@
+﻿using static EupDriversInfo.Models.EupDriverInfo.DriverInfoobj;
+using static EupDriversInfo.Models.EupDriverInfo;
+
+namespace EupDriversInfo.Models
+{
+    public class DriverSearch : BaseDrv
+    {
+        public string? driverName { get; set; }
+
+        public string? account { get; set; }
+        public List<DriverItem>? DataList { get; set; }
+
+        public class DriverItem
+        {
+            /// <summary>
+            /// 駕駛名稱
+            /// </summary>
+            public string? driverName { get; set; }
+            /// <summary>
+            /// 登入帳號
+            /// </summary>
+            public string? account { get; set; }
+            /// <summary>
+            /// 最後登入時間
+            /// </summary>
+            public DateTime? lastLoginTime { get; set; }
+        }
+
+        public bool GetDrvs(DriverSearch IDriverSearch)
+        {
+            return true;
+        }
+    }
+}
