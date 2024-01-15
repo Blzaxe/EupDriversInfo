21717bcf02f8cf6db32071cdc87bcab759298e94
blaze <blazezhang@liontravel.com>
2024-01-15 15:13:34 +0800

    查詢方法增設自定義查詢條件

--- Models/Drivers.cs
+++ Models/Drivers.cs
@@ -2,6 +2,8 @@
 using static EupDriversInfo.Models.EupDriverInfo;
 using System.Data.SqlClient;
 using Dapper;
+using static EupDriversInfo.Models.DriverSearch;
+using System.Security.Principal;
 
 namespace EupDriversInfo.Models
 {
@@ -15,10 +17,44 @@ namespace EupDriversInfo.Models
 
             try
             {
+                var sqlwhere = "WHERE 1 = 1";
+                Dictionary<string, object> dict = new ();               
+                if (!string.IsNullOrWhiteSpace(IDriverSearch.account))
+                {
+                    sqlwhere += Environment.NewLine + "AND account Like @Account";
+                    dict.Add("Account", $"{IDriverSearch.account}%");
+                }
+                if (!string.IsNullOrWhiteSpace(IDriverSearch.driverName))
+                {
+                    sqlwhere += Environment.NewLine + "AND driverName Like @DriverName";
+                    dict.Add("driverName", $"%{IDriverSearch.driverName}%");
+                }
+                var parameters = new DynamicParameters(dict);
                 using SqlConnection conn = new(_connectString);
 
-                var sql = "SELECT * FROM dbo.Driver;";
-                IResult = conn.Query<DriverSearch.DriverItem>(sql).ToList();
+                var sql = "SELECT * FROM dbo.Driver";
+
+                sql += Environment.NewLine + sqlwhere;
+                IResult = conn.Query<DriverSearch.DriverItem>(sql, parameters).ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return IResult;
+        }
+
+        public DriverItem GetDrv(int id)
+        {
+            DriverItem IResult = new();
+
+            try
+            {
+                using SqlConnection conn = new(_connectString);
+
+                var sql = "SELECT * FROM dbo.Driver WHERE id = @id;";
+                IResult = conn.QuerySingle<DriverItem>(sql, new { id });
             }
             catch (Exception ex)
             {
