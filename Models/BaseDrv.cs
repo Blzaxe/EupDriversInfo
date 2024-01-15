f6be0973412329a56142fce6c829302101acef34
blaze <blazezhang@liontravel.com>
2024-01-15 14:07:51 +0800

    設定值屬性調整為公開

--- Models/BaseDrv.cs
+++ Models/BaseDrv.cs
@@ -8,7 +8,7 @@
         //public readonly string _connectString = "Server=ITTC-04509-0050\\SQLEXPRESS;Database=EupData;User ID=sa;Password=ssmsLion220090;";
         public readonly string _connectString = string.Empty;
 
-        private readonly IConfiguration _config;
+        public readonly IConfiguration _config;
         public BaseDrv(IConfiguration config)
         {
             _config = config;
